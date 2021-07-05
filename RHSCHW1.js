38a0852e6cac48e76d3ea79023560be35575fa9e
Ryan Harris <RyanHarr33@gmail.com>
2021-07-05 12:57:33 -0500

    Add initial file

---
+++ RHSCHW1.js
@@ -0,0 +1,6 @@
+let pizzaPlace = "Ryan's Pizza Magic";
+let numberOfToppings = 5;
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+let str = `Hi! We are ${pizzaPlace} and we offer ${numberOfToppings} toppings and much more!`;
+console.log(str);
