c0fef5a37bea95fdd210e59291c130b821889131
Ryan Harris <RyanHarr33@gmail.com>
2021-07-06 22:04:57 -0500

    Activity update

---
+++ ClassActivities/2.3.7-WhileLoops.js
@@ -0,0 +1,12 @@
+// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100 // create a variable to represent the current number
+
+// create a variable to represent the current total
+
+// write a while loop that sums the numbers from 1 to 100
+
+let x = 1;
+let total = 0;
+while (x <= 100) {total += x;
+x++;}
+
+console.log(total);
--- RHSCHW1.js
+++
@@ -1,6 +0,0 @@
-let pizzaPlace = "Ryan's Pizza Magic";
-let numberOfToppings = 5;
-console.log(pizzaPlace);
-console.log(numberOfToppings);
-let str = `Hi! We are ${pizzaPlace} and we offer ${numberOfToppings} toppings and much more!`;
-console.log(str);
