33d0a86b2aa7b03369abeb4aa9e370317b58975c
Ryan Harris <RyanHarr33@gmail.com>
2021-07-18 18:38:52 -0500

    updates

---
+++ SCHomework/HomeworkWeek1.js
@@ -0,0 +1,8 @@
+let pizzaPlace = "Ryan's Pizza Magic";
+let numberOfToppings = 5;
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof "Ryan's Pizza Magic");
+console.log(typeof 5);
+let str = `Hi! We are ${pizzaPlace} and we offer ${numberOfToppings} toppings and much more!`;
+console.log(str);
